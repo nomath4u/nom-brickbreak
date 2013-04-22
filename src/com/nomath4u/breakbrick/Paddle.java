b3f6f4662caa288002eb6a647e856fdf7b4484d2
Chris Harper <nomath4u@gmail.com>
2013-04-21 17:37:34 -0700

    Moved bar to the bottom and created a unit so it will move all the way across

--- src/com/nomath4u/breakbrick/Paddle.java
+++ src/com/nomath4u/breakbrick/Paddle.java
@@ -4,24 +4,38 @@ package com.nomath4u.breakbrick;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Handler;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class Paddle {
 	
-	public Rect selfimage;
+	public RectF selfimage;
 	public Paint selfstyle;
 	private Handler timer;
 	private Runnable moveSelf;
 	private SensorManager mSensorManager;
 	private int adcval;
+	private float maxval;
+	private int screenwidth;
+	private int screenheight;
+	private float unit;
+	private int paddlewidth;
+	private int paddleheight;
 	
 	Paddle(Context context){
-		this.selfimage = new Rect(0,0, 100, 100); //set coordinates to upper left
+		this.selfimage = new RectF(0,0, 100, 100); //set coordinates to upper left
+		
+		paddlewidth = 30;
+		paddleheight = 10;
 		
 		/*Set Paint Style*/
 		this.selfstyle = new Paint();
@@ -42,7 +56,22 @@ public class Paddle {
 			}
 		};
 		mSensorManager.registerListener(tmp,adcsensor, 1000000000);
+		maxval = adcsensor.getMaximumRange();
 		
+		/*Get Window dimensions*/
+		Point size = new Point();
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+		      wm.getDefaultDisplay().getSize(size);
+		      screenwidth = size.x;
+		      screenheight = size.y; 
+		    }else{
+		      Display d = wm.getDefaultDisplay(); 
+		      screenwidth = d.getWidth(); 
+		      screenheight = d.getHeight(); 
+		    }
+		
+		setUnit();
 		
 		/*Set up moving self */
 		timer = new Handler();
@@ -59,7 +88,13 @@ public class Paddle {
 	}
 	
 	public void move(){
-		selfimage.set((adcval*-10) + 90, 0, (adcval*-10)+110, 10); 
+		
+		/*Get these values to be smoother and make it so screenheight is the bottom in manifest*/
+		float top = 900;
+		float left = (adcval  * unit * -1) + (screenwidth/2) - (paddlewidth/2);
+		float right = (adcval * unit * -1)+ (screenwidth/2) + (paddlewidth/2);
+		float bottom = screenheight;
+		selfimage.set(left, top, right , bottom); 
 	}
 	
 	public void startMoving(){
@@ -68,5 +103,9 @@ public class Paddle {
 	
 	public void stopMoving(){
 		timer.removeCallbacks(moveSelf);
+	}
+	
+	private void setUnit(){
+		unit = screenwidth / maxval;
 	}
 }
