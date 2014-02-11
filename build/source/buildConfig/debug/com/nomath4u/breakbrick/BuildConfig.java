d8003a51e30fd80f92f173fecf2be78f11d28d59
Chris <harperc@onid.orst.edu>
2014-02-10 23:09:13 -0800

    Migrated to gradle

---
+++ build/source/buildConfig/debug/com/nomath4u/breakbrick/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nomath4u.breakbrick;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.nomath4u.breakbrick";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+}
--- gen/com/nomath4u/breakbrick/R.java
+++ gen/com/nomath4u/breakbrick/R.java
@@ -10,36 +10,43 @@ package com.nomath4u.breakbrick;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int opaque_red=0x7f050000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int breakbrickicon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button=0x7f080002;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080003;
-        public static final int button3=0x7f080000;
-        public static final int menu_settings=0x7f080005;
-        public static final int ratingBar=0x7f080004;
+        public static final int button=0x7f090001;
+        public static final int button1=0x7f090004;
+        public static final int button2=0x7f090003;
+        public static final int button3=0x7f090002;
+        public static final int menu_settings=0x7f090005;
+        public static final int score_view=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int pause_screen=0x7f030001;
-        public static final int test2=0x7f030002;
+        public static final int game_overlay=0x7f030001;
+        public static final int main_menu=0x7f030002;
+        public static final int pause_screen=0x7f030003;
+        public static final int test2=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class raw {
         public static final int blip1=0x7f040000;
         public static final int blip2=0x7f040001;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int game_over_message=0x7f050004;
-        public static final int game_over_title=0x7f050003;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int test=0x7f050005;
+        public static final int app_name=0x7f060000;
+        public static final int game_over_message=0x7f060004;
+        public static final int game_over_title=0x7f060003;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int pause=0x7f060006;
+        public static final int test=0x7f060005;
     }
     public static final class style {
         /** 
@@ -63,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
